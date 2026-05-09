787c0138c3f321463be83cf2ff54b1b46e35e1a7
jhon-dev <navarro.jhonllyod2005@gmail.com>
2026-05-09 17:18:29 +0800

    added .env

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,25 +1,27 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import { getDatabase } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+import { getAuth } from "firebase/auth"
+import { getDatabase } from "firebase/database"
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase config using ENV
 const firebaseConfig = {
-  apiKey: "AIzaSyAGO8dy8MHUuvqSPbu4gSNd3FOgOTonlBM",
-  authDomain: "rent-a-place-system.firebaseapp.com",
-  projectId: "rent-a-place-system",
-  storageBucket: "rent-a-place-system.firebasestorage.app",
-  messagingSenderId: "1072658093898",
-  appId: "1:1072658093898:web:2bfeae191789c1cdd993b3",
-  measurementId: "G-H9L9XMT88F"
-};
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getDatabase(app);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig)
+
+// Services
+export const auth = getAuth(app)
+export const db = getDatabase(app)
+
+const analytics = getAnalytics(app)
+
+export default app
